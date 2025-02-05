8c94ef65ff7b448e3ffdf1cee84aa1fa2ee1f4f3
AntonioTri <antoniotrid@gmail.com>
2025-02-05 18:47:42 +0100

    Custom Command initialization

---
+++ ciao.py
@@ -0,0 +1,5 @@
+import sys
+
+nome = sys.argv[1] if len(sys.argv) > 1 else "Utente"
+
+print(f"Ciao {nome}!")
---
+++ saluta.bat
@@ -0,0 +1,2 @@
+@echo off
+python C:\Users\anton\Documents\Programmi_in_VS_Code\ComandiCustom\ciao.py %*
